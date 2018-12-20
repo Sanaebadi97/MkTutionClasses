3a3946440b0837085339e2432c9e3c05ac7627b1
Sana Ebadi <sanaebadi97@gmail.com>
2018-12-20 07:37:01 -0500

    Splash Screen Complete

--- app/src/main/java/sanaebadi/info/teacherhandler/activity/SplashScreenActivity.kt
+++ app/src/main/java/sanaebadi/info/teacherhandler/activity/SplashScreenActivity.kt
@@ -1,13 +1,40 @@
 package sanaebadi.info.teacherhandler.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import sanaebadi.info.teacherhandler.R
+import sanaebadi.info.teacherhandler.databinding.ActivitySplashScreenBinding
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : BaseActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var animation: Animation
+
+    private val SPLASH_TIME_OUT: Long = 2000
+    private var handler: Handler? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+
+        handler=Handler()
+        animation = AnimationUtils.loadAnimation(this, R.anim.transition)
+        binding.imgLogo.animation = animation
+        binding.txtSplash.animation = animation
+
+        handler!!.postDelayed({
+            kotlin.run {
+                val intent = Intent(applicationContext, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }, SPLASH_TIME_OUT)
+
+
     }
 }
